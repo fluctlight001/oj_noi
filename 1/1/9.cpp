a8cdf1760b90b56d8a74819869a01cbaf67a5534
fluctlight <609804988@qq.com>
2020-08-11 15:59:02 +0800

    add 1.1.9 1.1.10

---
+++ 1/1/9.cpp
@@ -0,0 +1,17 @@
+#include <stdio.h>
+int main (){
+    // freopen("read","r",stdin);
+    char a;
+    scanf("%c",&a);
+    for(int i=0;i<=2;i++){
+        for(int j=1-i;j>=0;j--) printf(" ");
+        for(int k=2*i;k>=0;k--) printf("%c",a);
+        printf("\n");
+    }
+    for(int i=1;i>=0;i--){
+        for(int j=1-i;j>=0;j--) printf(" ");
+        for(int k=2*i;k>=0;k--) printf("%c",a);
+        if(i!=0) printf("\n");
+    }
+    return 0;
+}

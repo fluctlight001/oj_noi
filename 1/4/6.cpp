49ea464a3dd2ff8701542ca3a6158d61a57c2901
fluctlight001 <fluctlight@qq.com>
2020-08-30 15:56:26 +0800

    add 6-7 in 1.4

---
+++ 1/4/6.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+  int a;
+  scanf("%d",&a);
+  if(a > 9 && a < 100)printf("1");
+  else printf("0");
+  return 0;
+}
---
+++ 1/4/7.cpp
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main(){
+  int a,b;
+  scanf("%d%d",&a,&b);
+  if(a>=10 || b>=20)printf("1");
+  else printf("0");
+  return 0;
+}
